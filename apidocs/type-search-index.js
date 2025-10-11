a3c894332ba1a8f7513d786d2483ec7609a399b2
Slawomir Jaranowski <s.jaranowski@gmail.com>
2025-10-11 16:45:33 +0200

    Site checkin for project Plexus Testing

---
+++ apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html","k":"18"},{"p":"org.codehaus.plexus.testing","l":"PlexusExtension"},{"p":"org.codehaus.plexus.testing","l":"PlexusTest","k":"8"}];updateSearchResults();
